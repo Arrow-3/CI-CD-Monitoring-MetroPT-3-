8baddfda102710f7df7aed530b59511164209713
Arya Shah <142084719+Arrow-3@users.noreply.github.com>
2026-06-22 12:39:11 +0200

    Add script to consume messages from Kafka stream

---
+++ scripts/consume_raw.py
@@ -0,0 +1,8 @@
+from prism import kafka_utils
+from prism.config.settings import get_consumer_group_id
+
+c = kafka_utils.get_consumer("raw_data_stream", get_consumer_group_id("debug_reader"))
+for i, msg in enumerate(c):
+    print(msg.value)
+    if i >= 4:
+        break
