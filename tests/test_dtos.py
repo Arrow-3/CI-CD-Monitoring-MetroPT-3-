de25231ee750590a4df98e2157a34f05c5876f11
Arya Shah <142084719+Arrow-3@users.noreply.github.com>
2026-06-23 16:32:40 +0200

    Delete tests directory

--- tests/test_dtos.py
+++
@@ -1,8 +0,0 @@
-from prism.dtos import RawDataDTO
-
-def test_rawdata_roundtrip():
-    dto = RawDataDTO(ts="2020-04-18T00:00:00+00:00", product_id="APU",
-                     tool_id="metro-1", raw={"TP2": 1.0, "Motor_current": 3.4})
-    again = RawDataDTO.from_json(dto.to_json())
-    assert again == dto
-    assert again.to_ndarray().tolist() == [1.0, 3.4]
